8b3975460094f2f920f754c482cbef51172c42a2
Jessica Sampaio-Herlitz <102844641+jessampaio@users.noreply.github.com>
2024-10-14 15:14:26 -0400

    project-1 done

---
+++ project-1/project-1Tests/project_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  project_1Tests.swift
+//  project-1Tests
+//
+//  Created by Jessica Sampaio-Herlitz on 10/14/24.
+//
+
+import Testing
+@testable import project_1
+
+struct project_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
